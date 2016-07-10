f70ac0c892e295bebcd8b682ac6b2b293a97e492
Christian Muñoz <christian.munoz@flow.io>
2016-07-10 12:11:28 -0400

    Fix unresolvable paths in entry file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,2 @@
 export { default as fetch } from './components/fetch';
-export { default as Spinner } from './components/spinner';
-export { default as Glitch } from './components/glitch';
 export { default as fetchRouteData } from './utilities/fetch-route-data';
