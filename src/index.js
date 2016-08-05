6f34ea88d7ab00b436d2e11058e2aedbcea1cd1d
Christian Munoz <ctumolosus@gmail.com>
2016-08-05 08:28:00 -0400

    :joy: Export React Router middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export fetch from './components/FetchContainer';
 export fetchAsyncState from './utilities/fetchAsyncState';
 export FetchProvider from './components/FetchProvider';
+export useFetch from './utilities/useFetch';
