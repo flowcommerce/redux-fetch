f2316d6d2b26ad76392704cd6436cdc09b9df94b
Christian Muñoz <christian.munoz@flow.io>
2017-01-05 11:33:16 -0500

    Slight refactor to fetch aggregator utility

--- src/utilities/fetchAsyncState.js
+++ src/utilities/fetchAsyncState.js
@@ -1,14 +1,19 @@
 /**
- * An utility that returns a promise that is resolved after all data requirements for fetch
- * containers are fulfilled or rejected otherwise.
- * @param {Object} store - An instance of the redux store used in your application.
- * @param {Object} renderProps - An object with non-router specific properties, typically injected
- * into RouterContext.
+ * An utility that returns a promise that is resolved after all data
+ * requirements for fetch containers are fulfilled or rejected otherwise.
+ * @param {Object} store An instance of the Redux store used in your application.
+ * @param {Object} renderProps An object with non-router specific properties,
+ * typically injected into `RouterContext`.
  * @return {Promise}
  */
 export default function fetchAsyncState({ dispatch, getState }, renderProps) {
-  return Promise.all(renderProps.components
-    // Assign a default value to prevent errors when a component is undefined.
-    .filter(({ getAsyncState } = {}) => getAsyncState)
-    .map(({ getAsyncState }) => getAsyncState(dispatch, getState, renderProps)));
+  const promises = renderProps.components
+    // filter falsy components
+    .filter(component => component)
+    // filter components that haven't been decorated
+    .filter(component => component.getAsyncState)
+    // fetch data requirements and store promises
+    .map(component => component.getAsyncState(dispatch, getState, renderProps));
+
+  return Promise.all(promises);
 }
