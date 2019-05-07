c3423bac70f9b6d23a165d2f3224a837797876fe
Gareth Dunne <gareth@flow.io>
2019-05-07 13:29:19 +0100

    Added response into formatError utility

--- src/formatError.js
+++ src/formatError.js
@@ -4,6 +4,7 @@ export default function formatError(error) {
   if (!isError(error)) return error;
   return {
     message: error.message,
+    response: error.response,
     name: error.name,
     stack: error.stack,
   };
