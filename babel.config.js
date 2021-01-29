68414f93518ce6f12c1f16762243c7e1fce77759
Christian Muñoz <christian.munoz@flow.io>
2021-01-29 13:40:23 -0500

    Fix issues with Babel transpilation

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,23 @@
+const { version: babelRuntimeVersion } = require('@babel/runtime/package.json');
+
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', {
+      // Exclude transforms that make all code slower.
+      // See https://github.com/facebook/create-react-app/pull/5278
+      exclude: ['transform-typeof-symbol'],
+    }],
     '@babel/preset-react',
   ],
   plugins: [
     ['@babel/plugin-transform-runtime', {
       corejs: false,
+      helpers: true,
       regenerator: false,
+      // By default, babel assumes babel/runtime version 7.0.0-beta.0,
+      // explicitly resolving to match the provided helper functions.
+      // https://github.com/babel/babel/issues/10261
+      version: babelRuntimeVersion,
     }],
     '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-proposal-object-rest-spread', {
